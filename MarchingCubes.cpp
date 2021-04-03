70ac682009c492c3ddf3dd3815a840d16d5f4b30
Conner <connertenn@gmail.com>
2021-04-02 22:11:06 -0400

    Added marching cubes algorithm

--- MarchingCubes.cpp
+++ MarchingCubes.cpp
@@ -21,8 +21,50 @@ float SignedDistance(vec3 p)
 
 #include <vector>
 
-std::vector<vec3> MarchingCubes()
+std::vector<vec3[3]> MarchingCubes()
 {
-	return std::vector<vec3>();
+	std::vector<vec3[3]> triangleList;
+
+	const float dimensions = 5.0f;
+	const float resolution = 100.0f;
+
+	const int bounds = dimensions*resolution;
+	for (int x=-bounds; x<=bounds; x++)
+	{
+		for (int y=-bounds; y<=bounds; y++)
+		{
+			for (int z=-bounds; z<=bounds; z++)
+			{
+				vec3 pos = vec3(x/resolution, y/resolution, z/resolution);
+
+				TRIANGLE triangles[5];
+				GRIDCELL grid;
+				grid.p[0] = vec3(pos.x-0.5, pos.y-0.5, pos.z-0.5);
+				grid.p[1] = vec3(pos.x+0.5, pos.y-0.5, pos.z-0.5);
+				grid.p[2] = vec3(pos.x+0.5, pos.y-0.5, pos.z+0.5);
+				grid.p[1] = vec3(pos.x-0.5, pos.y-0.5, pos.z+0.5);
+
+				grid.p[0] = vec3(pos.x-0.5, pos.y+0.5, pos.z-0.5);
+				grid.p[1] = vec3(pos.x+0.5, pos.y+0.5, pos.z-0.5);
+				grid.p[2] = vec3(pos.x+0.5, pos.y+0.5, pos.z+0.5);
+				grid.p[1] = vec3(pos.x-0.5, pos.y+0.5, pos.z+0.5);
+
+
+				for (int i=0; i<8; i++)
+				{
+					grid.val[i] = SignedDistance(grid.p[i]);
+				}
+
+				int numtri = Polygonise(grid, 0.0, triangles);
+
+				for (int i=0; i<numtri; i++)
+				{
+					triangleList.push_back(triangles[i].p);
+				}
+			}
+		}
+	}
+
+	return triangleList;
 }
 
