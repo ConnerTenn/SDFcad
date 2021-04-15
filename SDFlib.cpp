d195c34a310369e4c6370f38234f101d08d70294
Conner <connertenn@gmail.com>
2021-04-15 07:00:51 -0400

    Updated names and added primitive functions

--- SDFlib.cpp
+++ SDFlib.cpp
@@ -3,71 +3,27 @@
 #include "SDFlib.hpp"
 
 
-vec3 translate(vec3 pos, vec3 move)
-{
-	return pos - move;
-}
-
-float sdSphere(vec3 pos, float r)
-{
-	return length(pos) - r;
-}
-
-float sdBox(vec3 p, vec3 b)
-{
-	vec3 d = abs(p) - b;
-	return min(max(d.x,max(d.y,d.z)),0.0f) + length(max(d, vec3(0.0f)));
-}
-
-float sdBoxFast(vec3 pos, vec3 l)
-{
-	vec3 b = abs(pos) - l;
-	return max(max(b.x, b.y), b.z);
-}
-
-
-float sdUnion(float d0, float d1)
-{
-	return min(d0, d1);
-}
-
-float sdInter(float d0, float d1)
-{
-	return max(d0, d1);
-}
-
-float sdCappedCylinder(vec3 pos, float h, float r)
-{
-	float l = sqrt(pow(pos.x,2.0f) + pow(pos.z,2.0f));
-	float dx = abs(l) - r;
-	float dy = abs(pos.y) - h;
-	return min(max(dx,dy),0.0f) + sqrt(pow(max(dx,0.0f),2.0f) + pow(max(dy,0.0f),2.0f));
-}
-
-
-
-
 //== Objects ==
 
 SDF3 Sphere(float radius)
 {
-	return SDF3(new SDSphere(radius));
+	return SDF3(new SDFSphere(radius));
 }
 SDF3 SphereD(float diameter)
 {
-	return SDF3(new SDSphere(diameter/2.0f));
+	return SDF3(new SDFSphere(diameter/2.0f));
 }
 SDF3 Box(vec3 size)
 {
-	return SDF3(new SDBox(size));
+	return SDF3(new SDFBox(size));
 }
 SDF3 Cube(float size)
 {
-	return SDF3(new SDBox(vec3(size)));
+	return SDF3(new SDFBox(vec3(size)));
 }
 SDF3 Cylinder(float height, float radius)
 {
-	return SDF3(new SDCylinder(height, radius));
+	return SDF3(new SDFCylinder(height, radius));
 }
 
 
@@ -75,19 +31,19 @@ SDF3 Cylinder(float height, float radius)
 
 SDF3 Translate(SDF3 object, vec3 move)
 {
-	return SDF3(new SDTranslate(object, move));
+	return SDF3(new SDFTranslate(object, move));
 }
 SDF3 Union(SDF3 object1, SDF3 object2)
 {
-	return SDF3(new SDUnion(object1, object2));
+	return SDF3(new SDFUnion(object1, object2));
 }
 SDF3 Difference(SDF3 object1, SDF3 object2)
 {
-	return SDF3(new SDDifference(object1, object2));
+	return SDF3(new SDFDifference(object1, object2));
 }
 SDF3 Intersect(SDF3 object1, SDF3 object2)
 {
-	return SDF3(new SDIntersect(object1, object2));
+	return SDF3(new SDFIntersect(object1, object2));
 }
 SDF3 Transform(SDF3 object, mat4 matrix)
 {
--- SDFlib.hpp
+++ SDFlib.hpp
@@ -5,14 +5,51 @@
 #include "Common.hpp"
 
 
-vec3 translate(vec3 pos, vec3 move);
-float sdSphere(vec3 pos, float r);
-float sdBox(vec3 p, vec3 b);
-float sdBoxFast(vec3 pos, vec3 l);
-float sdCappedCylinder(vec3 pos, float h, float r);
+inline float SdfSphere(vec3 pos, float radius)
+{
+	return length(pos) - radius;
+}
+inline float SdfRoundBox(vec3 pos, vec3 size, float radius)
+{
+	vec3 d = abs(pos) - size;
+	return min(max(d.x,max(d.y,d.z)),0.0f) + length(max(d, vec3(0.0f))) - radius;
+}
+inline float SdfBox(vec3 pos, vec3 size)
+{
+	vec3 b = abs(pos) - size;
+	return max(max(b.x, b.y), b.z);
+}
+inline float SdfCylinder(vec3 pos, float height, float radius)
+{
+		float l = sqrt(pow(pos.x,2.0f) + pow(pos.z,2.0f));
+		float dx = abs(l) - radius;
+		float dy = abs(pos.y) - height;
+		return min(max(dx,dy),0.0f) + sqrt(pow(max(dx,0.0f),2.0f) + pow(max(dy,0.0f),2.0f));
+}
 
-float sdUnion(float d0, float d1);
-float sdInter(float d0, float d1);
+
+inline vec3 SdfTranslate(vec3 pos, vec3 move)
+{
+	return pos - move;
+}
+inline float SdfUnion(float d0, float d1)
+{
+	return min(d0, d1);
+}
+inline float SdfDifference(float d0, float d1)
+{
+	return max(d0, -d1);
+}
+inline float SdfIntersect(float d0, float d1)
+{
+	return max(d0, d1);
+}
+inline vec3 SdfTransform(vec3 pos, mat4 matrix)
+{
+	vec4 res = matrix * vec4(pos.x, pos.y, pos.z, 1);
+	return vec3(res.x, res.y, res.z);
+}
+
 
 class _SDF3
 {
@@ -141,77 +178,73 @@ public:
 //==     Objects     ==
 //=====================
 
-class SDSphere : public _SDF3
+class SDFSphere : public _SDF3
 {
 	float Radius;
 public:
-	SDSphere(float radius) : _SDF3()
+	SDFSphere(float radius) : _SDF3()
 	{
-		// std::cout << "+ SDSphere\n";
+		// std::cout << "+ SDFSphere\n";
 		Radius = radius;
 	}
-	~SDSphere() {} //{ std::cout << "- SDSphere\n"; }
+	~SDFSphere() {} //{ std::cout << "- SDFSphere\n"; }
 
 
 	_SDF3 *Duplicate() const
 	{
-		return new SDSphere(Radius);
+		return new SDFSphere(Radius);
 	}
 
 	float operator()(vec3 pos)
 	{
-		return length(pos) - Radius;
+		return SdfSphere(pos, Radius);
 	}
 };
 
-class SDBox : public _SDF3
+class SDFBox : public _SDF3
 {
 	vec3 Size;
 public:
-	SDBox(vec3 size) : _SDF3()
+	SDFBox(vec3 size) : _SDF3()
 	{
-		// std::cout << "+ SDBox\n";
+		// std::cout << "+ SDFBox\n";
 		Size = size/2.0f;
 	}
-	~SDBox() {} //{ std::cout << "- SDBox\n"; }
+	~SDFBox() {} //{ std::cout << "- SDFBox\n"; }
 
 
 	_SDF3 *Duplicate() const
 	{
-		return new SDBox(Size);
+		return new SDFBox(Size);
 	}
 
 	float operator()(vec3 pos)
 	{
-		vec3 b = abs(pos) - Size;
-		return max(max(b.x, b.y), b.z);
+		return SdfBox(pos, Size);
 	}
 };
 
-class SDCylinder : public _SDF3
+class SDFCylinder : public _SDF3
 {
 	float Height;
 	float Radius;
 public:
-	SDCylinder(float height, float radius) : _SDF3()
+	SDFCylinder(float height, float radius) : _SDF3()
 	{
 		Height = height;
 		Radius = radius;
 	}
-	~SDCylinder() {}
+	~SDFCylinder() {}
 
 
 	_SDF3 *Duplicate() const
 	{
-		return new SDCylinder(Height, Radius);
+		return new SDFCylinder(Height, Radius);
 	}
 
 	float operator()(vec3 pos)
 	{
-		float l = sqrt(pow(pos.x,2.0f) + pow(pos.z,2.0f));
-		float dx = abs(l) - Radius;
-		float dy = abs(pos.y) - Height;
-		return min(max(dx,dy),0.0f) + sqrt(pow(max(dx,0.0f),2.0f) + pow(max(dy,0.0f),2.0f));
+		return SdfCylinder(pos, Height, Radius);
 	}
 };
 
@@ -220,100 +253,99 @@ public:
 //==    Modifiers    ==
 //=====================
 
-class SDTranslate : public _SDF3
+class SDFUnion : public _SDF3
+{
+	SDF3 Object1;
+	SDF3 Object2;
+public:
+	SDFUnion(SDF3 object1, SDF3 object2) : _SDF3()
+	{
+		// std::cout << "+ SDFUnion\n";
+		Object1 = object1;
+		Object2 = object2;
+	}
+	~SDFUnion() {} //{ std::cout << "- SDFUnion\n"; }
+
+	_SDF3 *Duplicate() const
+	{
+		return new SDFUnion(Object1, Object2);
+	}
+
+	float operator()(vec3 pos)
+	{
+		return SdfUnion((*Object1.Object)(pos), (*Object2.Object)(pos));
+	}
+};
+
+class SDFDifference : public _SDF3
+{
+	SDF3 Object1;
+	SDF3 Object2;
+public:
+	SDFDifference(SDF3 object1, SDF3 object2) : _SDF3()
+	{
+		// std::cout << "+ SDFDifference\n";
+		Object1 = object1;
+		Object2 = object2;
+	}
+	~SDFDifference() {} //{ std::cout << "- SDFDifference\n"; }
+
+	_SDF3 *Duplicate() const
+	{
+		return new SDFDifference(Object1, Object2);
+	}
+
+	float operator()(vec3 pos)
+	{
+		return SdfDifference((*Object1.Object)(pos), (*Object2.Object)(pos));
+	}
+};
+
+class SDFIntersect : public _SDF3
+{
+	SDF3 Object1;
+	SDF3 Object2;
+public:
+	SDFIntersect(SDF3 object1, SDF3 object2) : _SDF3()
+	{
+		// std::cout << "+ SDFIntersect\n";
+		Object1 = object1;
+		Object2 = object2;
+	}
+	~SDFIntersect() {} //{ std::cout << "- SDFIntersect\n"; }
+
+	_SDF3 *Duplicate() const
+	{
+		return new SDFIntersect(Object1, Object2);
+	}
+
+	float operator()(vec3 pos)
+	{
+		return SdfIntersect((*Object1.Object)(pos), (*Object2.Object)(pos));
+	}
+};
+
+class SDFTranslate : public _SDF3
 {
 	SDF3 Object;
 	vec3 Move;
 public:
-	SDTranslate(SDF3 object, vec3 move) : _SDF3()
+	SDFTranslate(SDF3 object, vec3 move) : _SDF3()
 	{
-		// std::cout << "+ SDTranslate\n";
+		// std::cout << "+ SDFTranslate\n";
 		Object = object;
 		Move = move;
 	}
-	~SDTranslate() {} //{ std::cout << "- SDTranslate\n"; }
+	~SDFTranslate() {} //{ std::cout << "- SDFTranslate\n"; }
 
 	_SDF3 *Duplicate() const
 	{
-		return new SDTranslate(Object, Move);
+		return new SDFTranslate(Object, Move);
 	}
 
 	float operator()(vec3 pos)
 	{
-		// std::cout << "Translate()\n";
-		return (*Object.Object)(pos - Move);
-	}
-};
-
-class SDUnion : public _SDF3
-{
-	SDF3 Object1;
-	SDF3 Object2;
-public:
-	SDUnion(SDF3 object1, SDF3 object2) : _SDF3()
-	{
-		// std::cout << "+ SDUnion\n";
-		Object1 = object1;
-		Object2 = object2;
-	}
-	~SDUnion() {} //{ std::cout << "- SDUnion\n"; }
-
-	_SDF3 *Duplicate() const
-	{
-		return new SDUnion(Object1, Object2);
-	}
-
-	float operator()(vec3 pos)
-	{
-		return min((*Object1.Object)(pos), (*Object2.Object)(pos));
-	}
-};
-
-class SDDifference : public _SDF3
-{
-	SDF3 Object1;
-	SDF3 Object2;
-public:
-	SDDifference(SDF3 object1, SDF3 object2) : _SDF3()
-	{
-		// std::cout << "+ SDDifference\n";
-		Object1 = object1;
-		Object2 = object2;
-	}
-	~SDDifference() {} //{ std::cout << "- SDDifference\n"; }
-
-	_SDF3 *Duplicate() const
-	{
-		return new SDDifference(Object1, Object2);
-	}
-
-	float operator()(vec3 pos)
-	{
-		return max((*Object1.Object)(pos), -(*Object2.Object)(pos));
-	}
-};
-
-class SDIntersect : public _SDF3
-{
-	SDF3 Object1;
-	SDF3 Object2;
-public:
-	SDIntersect(SDF3 object1, SDF3 object2) : _SDF3()
-	{
-		// std::cout << "+ SDIntersect\n";
-		Object1 = object1;
-		Object2 = object2;
-	}
-	~SDIntersect() {} //{ std::cout << "- SDIntersect\n"; }
-
-	_SDF3 *Duplicate() const
-	{
-		return new SDIntersect(Object1, Object2);
-	}
-
-	float operator()(vec3 pos)
-	{
-		return max((*Object1.Object)(pos), (*Object2.Object)(pos));
+		return (*Object.Object)(SdfTranslate(pos, Move));
 	}
 };
 
@@ -337,8 +369,7 @@ public:
 
 	float operator()(vec3 pos)
 	{
-		vec4 res = TransformMat * vec4(pos.x, pos.y, pos.z, 1);
-		return (*Object.Object)(vec3(res.x, res.y, res.z));
+		return (*Object.Object)(SdfTransform(pos, TransformMat));
 	}
 };
 
