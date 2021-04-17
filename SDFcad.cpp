b369e7d9cc89ba630e9c89814561af86b6991ff8
Conner <connertenn@gmail.com>
2021-04-16 22:34:12 -0400

    Adding runtime mesh reloading

--- SDFcad.cpp
+++ SDFcad.cpp
@@ -136,12 +136,12 @@ int main()
 	GLuint vertexbuffer;
 	glGenBuffers(1, &vertexbuffer);
 	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat)*numEntries, vertexData, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat)*numEntries, vertexData, GL_DYNAMIC_DRAW);
 	
 	GLuint normalbuffer;
 	glGenBuffers(1, &normalbuffer);
 	glBindBuffer(GL_ARRAY_BUFFER, normalbuffer);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat)*numEntries, normalData, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat)*numEntries, normalData, GL_DYNAMIC_DRAW);
 
 	free(vertexData);
 	free(normalData);
@@ -221,6 +221,67 @@ int main()
 			}
 		}
 
+		{
+			static int keylast = GLFW_RELEASE;
+			int key = glfwGetKey(Window, GLFW_KEY_R);
+
+			if (key == GLFW_PRESS && keylast == GLFW_RELEASE)
+			{
+				Time t1 = GetTime();
+				InitSignedDistance((char *)"SignedDistance.cpp");
+
+				std::cout << "Generating Marching Cubes...\n";
+				unsigned int numEntries;
+				UserConstructSignedDistance();
+				float *vertexData = MarchingCubes(&numEntries);
+				UserDestructSignedDistance();
+				std::cout << "Marching Cubes generated (" << numEntries/3 << " vertices) (" << (numEntries/3)/3 << " triangles)\n";
+
+				float *normalData = (float *)malloc(sizeof(float)*numEntries);
+				for (unsigned int i=0; i<numEntries; i+=3*3)
+				{
+					vec3 p1 = vec3(vertexData[i+0*3+0], vertexData[i+0*3+1], vertexData[i+0*3+2]);
+					vec3 p2 = vec3(vertexData[i+1*3+0], vertexData[i+1*3+1], vertexData[i+1*3+2]);
+					vec3 p3 = vec3(vertexData[i+2*3+0], vertexData[i+2*3+1], vertexData[i+2*3+2]);
+
+					vec3 n1 = cross(p2-p1, p3-p1);
+					// float n1mag = length(n1);
+					n1 = normalize(n1);
+
+					// vec3 n2 = cross(p1-p2, p3-p2);
+					// float n2mag = length(n2);
+					// n2 = normalize(n2);
+					
+					// vec3 n3 = cross(p1-p3, p2-p3);
+					// float n3mag = length(n3);
+					// n3 = normalize(n3);
+
+					normalData[i+0*3+0]=n1.x; normalData[i+0*3+1]=n1.y; normalData[i+0*3+2]=n1.z;
+					normalData[i+1*3+0]=n1.x; normalData[i+1*3+1]=n1.y; normalData[i+1*3+2]=n1.z;
+					normalData[i+2*3+0]=n1.x; normalData[i+2*3+1]=n1.y; normalData[i+2*3+2]=n1.z;
+				}
+				Time t2 = GetTime();
+
+				std::cout << "Total Time: " << DurationString(t1, t2) << "\n";
+
+				// GLuint vertexbuffer;
+				// glGenBuffers(1, &vertexbuffer);
+				glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+				glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat)*numEntries, vertexData, GL_DYNAMIC_DRAW);
+				
+				// GLuint normalbuffer;
+				// glGenBuffers(1, &normalbuffer);
+				glBindBuffer(GL_ARRAY_BUFFER, normalbuffer);
+				glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat)*numEntries, normalData, GL_DYNAMIC_DRAW);
+
+				free(vertexData);
+				free(normalData);
+				ShutdownSignedDistance();
+			}
+
+			keylast = key;
+		}
+
 
 		// Clear the screen
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -272,8 +333,8 @@ int main()
 		// Draw the triangles!
 		glDrawArrays(GL_TRIANGLES, 0, numEntries);
 
-		glDisableVertexAttribArray(0);
-		glDisableVertexAttribArray(1);
+		// glDisableVertexAttribArray(0);
+		// glDisableVertexAttribArray(1);
 
 		
 		// Swap buffers
